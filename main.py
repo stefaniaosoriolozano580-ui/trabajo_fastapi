d1ef52304b426e9785ef117e811adcdecfd51e72
stefaniaosoriolozano580-ui <stefaniaosoriolozano580@gmail.com>
2026-06-02 20:09:01 -0500

    Refactor FastAPI routes and update template responses

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-from datetime import datetime
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(request, "inicio.html", {"ano_actual": datetime.now().year})
+@app.get("/")
+def inicio(request: Request):
+    return templates.TemplateResponse("inicio.html", {"request": request})
 
-@app.get("/inicio", response_class=HTMLResponse)
-async def inicio(request: Request):
-    return templates.TemplateResponse(request, "inicio.html", {"ano_actual": datetime.now().year})
+@app.get("/productos")
+def productos(request: Request):
+    return templates.TemplateResponse("productos.html", {"request": request})
 
-@app.get("/productos", response_class=HTMLResponse)
-async def productos(request: Request):
-    return templates.TemplateResponse(request, "productos.html", {"ano_actual": datetime.now().year})
+@app.get("/cuidado-pelo")
+def cuidado(request: Request):
+    return templates.TemplateResponse("cuidado-pelo.html", {"request": request})
 
-# Ruta actualizada para cuidadopelo.html
-@app.get("/pelocuidado", response_class=HTMLResponse)
-async def pelocuidado(request: Request):
-    return templates.TemplateResponse(request, "cuidadopelo.html", {"ano_actual": datetime.now().year})
-
-@app.get("/contacto", response_class=HTMLResponse)
-async def contacto(request: Request):
-    return templates.TemplateResponse(request, "contacto.html", {"ano_actual": datetime.now().year})
-    
+@app.get("/contacto")
+def contacto(request: Request):
+    return templates.TemplateResponse("contacto.html", {"request": request})
