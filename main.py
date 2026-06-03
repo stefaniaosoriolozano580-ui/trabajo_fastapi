8444ad1692512da3ae86d5028e1c294803a7a97e
stefaniaosoriolozano580-ui <stefaniaosoriolozano580@gmail.com>
2026-06-02 20:39:44 -0500

    Refactor FastAPI routes to include year in context

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+from datetime import datetime
+
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def inicio(request: Request):
-    return templates.TemplateResponse("inicio.html", {"request": request})
+@app.get("/", response_class=HTMLResponse)
+async def inicio(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="inicio.html", context={"ano_actual": datetime.now().year},
+    )
 
-@app.get("/productos")
-def productos(request: Request):
-    return templates.TemplateResponse("productos.html", {"request": request})
 
-@app.get("/cuidado-pelo")
-def cuidado(request: Request):
-    return templates.TemplateResponse("cuidadopelo.html", {"request": request})
+@app.get("/inicio", response_class=HTMLResponse)
+async def inicio(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="inicio.html", context={"ano_actual": datetime.now().year},
+    )
 
-@app.get("/contacto")
-def contacto(request: Request):
-    return templates.TemplateResponse("contacto.html", {"request": request})
+
+@app.get("/productos", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="productos.html", context={"ano_actual": datetime.now().year},
+    )
+
+
+@app.get("/contacto", response_class=HTMLResponse)
+async def contacto(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="contacto.html", context={"ano_actual": datetime.now().year},
+    )
+
+@app.get("/cuidadopiel", response_class=HTMLResponse)
+async def contacto(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="cuidadopelo.html", context={"ano_actual": datetime.now().year},
+    )
